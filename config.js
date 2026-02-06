27b687fd18b86da32f519e3cce1c4c5450db92c9
Lyghtless <ahmedt.rasooli@gmail.com>
2026-02-05 20:24:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -43,14 +43,14 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        high: "EVEN MORE! 🚀💝",              // Shows when they go past 1000%
+        normal: "more! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get a big warm hug and a huge smooch!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
